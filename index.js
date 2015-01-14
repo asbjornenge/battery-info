d17b10fe53686ebd1e7a2afd647afd8bdcaa3a3d
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-14 09:32:29 +0100

    Cleanup

--- index.js
+++ index.js
@@ -20,17 +20,15 @@ function batteryInfo(battery, callback) {
 
     var emitter = walkdir(_path+'/', function(path, stat) {})
     emitter.on('file', function(file) {
-        //console.log('file', file)
         _files.push(file)
         readAndPopulate(file, _path, _info)
     })
     emitter.on('end', function() {
-        console.log('im done')
         _done = true
         check_complete()
     })
 
-    function readFileMaybe(fullpath) {
+    function tryReadFile(fullpath) {
         if (!fs.lstatSync(fullpath).isFile()) return null
         var data;
         try { data = fs.readFileSync(fullpath, 'utf-8') }
@@ -45,50 +43,14 @@ function batteryInfo(battery, callback) {
     function readAndPopulate(file, parent_path, root) {
         var objPath = file.split(parent_path)[1].slice(1)
         var firstObjPath = objPath.split('/')[0]
-        console.log('first', firstObjPath)
         if (objPath.indexOf('/') > 0) {
             if (typeof root[firstObjPath] == 'undefined') root[firstObjPath] = {}
             return readAndPopulate(file, parent_path+'/'+firstObjPath, root[firstObjPath])
         }
-        root[firstObjPath] = readFileMaybe(file) 
+        root[firstObjPath] = tryReadFile(file) 
         _files_read.push(objPath)
         check_complete()
     }
-
-    function buildJSON(keys, obj) {
-        obj = obj || {}
-        var parent = keys.shift()
-        if (keys.length > 0) { 
-            if (typeof obj[parent] != 'object') obj[parent] = {}; 
-            return buildJSON(keys, obj[parent]) 
-        }
-        else obj[parent] = parent
-        return obj
-    }
-
-    function readFiles(obj, parent_path) {
-        Object.keys(obj).forEach(function(key) {
-            if (typeof obj[key] == 'object') return readFiles(obj[key], parent_path+'/'+key)
-            var fullpath = parent_path+'/'+key
-            if (!fs.lstatSync(fullpath).isFile()) return obj[key] = null
-            try { obj[key] = fs.readFileSync(fullpath, 'utf-8') }
-            catch(e) { obj[key] = null }
-            if (typeof obj[key] == 'string') {
-                var newlines = obj[key].split('\n')
-                if (newlines.length == 2) obj[key] = newlines[0]
-            }
-        })
-    }
-
-    var paths = walkdir.sync(_path+'/').map(function(fullpath) {
-        return fullpath.split(_path)[1].split('/').slice(1)
-    }).reduce(function(coll, curr, index, arr) {
-        buildJSON(curr, coll)
-        return coll
-    }, {})
-
-    readFiles(paths, _path)
-    return paths 
 }
 
 module.exports = batteryInfo;
